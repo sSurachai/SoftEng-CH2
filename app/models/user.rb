b17f5e09e601e7d7d35234b937ed057bf563ede7
sSurachai <109776885+sSurachai@users.noreply.github.com>
2023-07-31 09:46:50 +0000

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_many :microposts
+    validates FILL_IN, presence: true # Replace FILL_IN withthe right code.
+    validates FILL_IN, presence: true # Replace FILL_IN withthe right code.
+end
